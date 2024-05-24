4fbdae79dbc27713a5cd1dda4e3cfc7d6b3e345d
Nathan Guenther <nathan.guenther.web@gmail.com>
2024-05-23 19:14:30 -0600

    updates to the app's dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
+import '@/app/ui/global.css';
+import { inter, lusitana } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${inter.variable} ${lusitana.variable} antialiased`}
+    >
       <body>{children}</body>
     </html>
   );
